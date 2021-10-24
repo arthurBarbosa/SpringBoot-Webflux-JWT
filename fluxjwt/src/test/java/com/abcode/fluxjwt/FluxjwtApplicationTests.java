69e903d8610714ae1de189666f4a1e212eb74075
Arthur Barbosa <arthurbruno03@gmail.com>
2021-10-24 12:27:44 -0500

    created project

---
+++ fluxjwt/src/test/java/com/abcode/fluxjwt/FluxjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcode.fluxjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
