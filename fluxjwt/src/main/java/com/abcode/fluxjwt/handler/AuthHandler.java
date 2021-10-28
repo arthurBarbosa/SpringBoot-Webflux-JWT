f56b8a4b3c78d725d7558d6c5632d7ad6702d0db
Arthur Barbosa <arthurbruno03@gmail.com>
2021-10-28 17:25:43 -0500

    created auth handler

--- fluxjwt/src/main/java/com/abcode/fluxjwt/handler/AuthHandler.java
+++ fluxjwt/src/main/java/com/abcode/fluxjwt/handler/AuthHandler.java
@@ -1,5 +1,8 @@
 package com.abcode.fluxjwt.handler;
 
+import com.abcode.fluxjwt.domain.User;
+import com.abcode.fluxjwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -9,7 +12,14 @@ import reactor.core.publisher.Mono;
 @Service
 public class AuthHandler {
 
+    @Autowired
+    private UserRepository userRepository;
+
     public Mono<ServerResponse> signUp(ServerRequest request) {
-        return ServerResponse.ok().body(BodyInserters.fromValue("OOoooopppaaaaaa!!!!"));
+        Mono<User> userMono = request.bodyToMono(User.class);
+
+        return userMono.map(u -> new User(u.getUsername(), u.getPassword()))
+                .flatMap(this.userRepository::save)
+                .flatMap(user -> ServerResponse.ok().body(user, User.class));
     }
 }
